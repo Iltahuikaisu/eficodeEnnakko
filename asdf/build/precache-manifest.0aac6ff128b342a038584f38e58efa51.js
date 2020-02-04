765ac9986e6db32d5214d2af5c343ae417eaf486
Aaro Läppäri <aaro.kurki@gmail.com>
2020-02-04 09:54:20 +0200

    questionable adds

---
+++ asdf/build/precache-manifest.0aac6ff128b342a038584f38e58efa51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ca391916ccc2b3adf6f3429de3bc4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a42dab35d382c4f72dbb",
+    "url": "/static/css/main.0db4f0ba.chunk.css"
+  },
+  {
+    "revision": "e55da11a21eaafa3130f",
+    "url": "/static/js/2.3dca08d0.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/static/js/2.3dca08d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42dab35d382c4f72dbb",
+    "url": "/static/js/main.f2a852ef.chunk.js"
+  },
+  {
+    "revision": "cdf041b3892fd22a989b",
+    "url": "/static/js/runtime-main.6c427cc5.js"
+  }
+]);
---
+++ asdf/www.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+app.use(express.static('./build'))
+const PORT = process.env.PORT || 3001
+
+function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500)
+    res.send("An error has occured or not.")
+}
+
+app.use(errorHandler)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
